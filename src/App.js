f5aece41246528e92f976ddc52e4b81f0aaba097
Tushantbhasker <tushant@tdgpartner.info>
2020-06-09 09:57:57 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,94 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 import axios from 'axios';
+import User from './components/users/User';
+import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    repos:[],
+    loading: false,
+    alert:null,
+    user:{}
   }
 
-  async componentDidMount() {
+  searchUsers = async text => {
     this.setState({loading:true});
-    const res = await axios.get('https://api.github.com/users');
-    this.setState({users:res.data, loading:false});
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}`);
+    this.setState({users:res.data.items, loading:false});
   }
-
+  //Single user
+  getUser = async (username) => {
+    this.setState({loading:true});
+    const res = await axios.get(`https://api.github.com/users/${username}`);
+    this.setState({user:res.data, loading:false});
+  }
+  //get user repo
+  getUserRepos = async (username) => {
+    this.setState({loading:true});
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc`);
+    this.setState({repos:res.data, loading:false});
+  }
+  clearUsers = () => {
+    this.setState({users:[], loading:false});
+  }
+  setAlert = (msg,type) =>{
+    this.setState({alert:{msg, type}});
+    setTimeout(()=>{
+      this.setState({alert:null})
+    },5000);
+  }
 
   render() {
     return (
+      <Router>
       <div className="App">
         <Navbar title="Github Finder"/>
         <div className='container'>
-         <Users loading={this.state.loading} users={this.state.users} />
+          <Alert alert={this.state.alert}/>
+          <Switch>
+            <Route
+              exact path='/'
+              render={props=> (
+                <Fragment>
+                  <Search 
+                    searchUsers={this.searchUsers}
+                    clearUsers={this.clearUsers}
+                    showUsers={this.state.users.length > 0 ? true:false}
+                    setAlert={this.setAlert}
+                    />
+                  <Users loading={this.state.loading} users={this.state.users} />
+                </Fragment>
+              )}
+            />
+            <Route 
+              exact path='/about'
+              component={About}
+            />
+            <Route
+              exact path='/user/:login'
+              render={props=> (
+                <User 
+                  {...props} 
+                  getUser={this.getUser}
+                  getUserRepos={this.getUserRepos}
+                  user={this.state.user}
+                  repos={this.state.repos}
+                  loading={this.state.loading}
+                />
+              )}
+            />
+          </Switch>
+        
          </div>
       </div>
+      </Router>
     );
   }
  
---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react';
+
+const About = props => {
+    return (
+      <Fragment>
+          <h1>About Page</h1>
+      </Fragment>
+    );  
+}
+
+export default About;
